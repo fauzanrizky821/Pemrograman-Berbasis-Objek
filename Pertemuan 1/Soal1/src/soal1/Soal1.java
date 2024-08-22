1c5facfa22ec020f2ecb12bb27906c753d75b8e1
Fauzan Rizky Ramadhan <fauzanrizky821@gmail.com>
2024-08-23 06:47:18 +0700

    add: Tugas pertemuan 1

---
+++ Pertemuan 1/Soal1/src/soal1/Soal1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package soal1;
+
+/**
+ *
+ * @author user
+ */
+public class Soal1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        byte angka1 = 125;
+        byte angka2 = 6;
+        byte hasil = (byte) (angka1+angka2);
+        
+        System.out.println("Hasil 1 " + hasil);
+    }
+    
+}
